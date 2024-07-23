3ab422002e67fce27e4e2fde709ff2ccd52df4f8
Szomti <piotrpiotr1285@gmail.com>
2024-07-23 23:16:52 +0200

    Base for storage

--- lib/storage/bookmarks/bookmark.dart
+++ lib/storage/bookmarks/bookmark.dart
@@ -1,4 +1,9 @@
 class Bookmark {
+  static const _idKey = 'id';
+  static const _textKey = 'text';
+  static const _ordinalKey = 'ordinal';
+  static const _chapterKey = 'chapter';
+
   final int id;
   final String text;
   final int ordinal;
@@ -10,9 +15,33 @@ class Bookmark {
     required this.ordinal,
     required this.chapter,
   });
+
+  Map<String, Object?> toJson() {
+    return {
+      _idKey: id,
+      _textKey: text,
+      _ordinalKey: ordinal,
+      _chapterKey: chapter.toJson(),
+    };
+  }
+
+  static Bookmark fromJson(Map<String, Object?> object) {
+    throw ('Not implemented yet');
+  }
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is Bookmark && runtimeType == other.runtimeType && id == other.id;
+
+  @override
+  int get hashCode => id.hashCode;
 }
 
 class Chapter {
+  static const _mainKey = 'main';
+  static const _subKey = 'sub';
+
   final int main;
   final int sub;
 
@@ -20,4 +49,11 @@ class Chapter {
     required this.main,
     required this.sub,
   });
+
+  Map<String, Object?> toJson() {
+    return {
+      _mainKey: main,
+      _subKey: sub,
+    };
+  }
 }
--- lib/storage/bookmarks/bookmarks.dart
+++ lib/storage/bookmarks/bookmarks.dart
@@ -1,13 +1,43 @@
 import 'dart:collection';
+import 'dart:convert';
+
+import 'package:shared_preferences/shared_preferences.dart';
 
 import 'bookmark.dart';
 
 class BookmarksStorage {
   static const _iterableKey = 'BOOKMARKS_ITERABLE';
+  static const _countKey = 'BOOKMARKS_COUNT';
+  static const _defaultCount = 0;
   static final instance = BookmarksStorage._();
   static final Iterable<Bookmark> _items = SplayTreeSet<Bookmark>();
+  static int count = _defaultCount;
 
   BookmarksStorage._();
 
   Iterable<Bookmark> get items => _items.toList();
+
+  Future<void> load() async {
+    final prefs = await SharedPreferences.getInstance();
+    count = prefs.getInt(_countKey) ?? _defaultCount;
+    final iterable = prefs.getStringList(_iterableKey) ?? [];
+    final tempItems = SplayTreeSet<Bookmark>();
+    for (final item in iterable) {
+      tempItems.add(
+        Bookmark.fromJson(json.decode(item) as Map<String, Object?>),
+      );
+    }
+  }
+
+  Future<void> save() async {
+    final prefs = await SharedPreferences.getInstance();
+    prefs.setInt(_countKey, count);
+    prefs.setStringList(_iterableKey, _bookmarksToSave().toList());
+  }
+
+  Iterable<String> _bookmarksToSave() sync* {
+    for (final bookmark in _items) {
+      yield json.encode(bookmark.toJson());
+    }
+  }
 }
